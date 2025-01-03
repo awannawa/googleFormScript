35ca0bd0a6d0871e01a70d5c22452cde363a61ef
Ahmad Rizqi <20770875+awannawa@users.noreply.github.com>
2025-01-03 15:33:28 +0700

    Create limitFormbyDate.js

---
+++ limitFormbyDate.js
@@ -0,0 +1,31 @@
+function closeForm() {
+
+const waktuCloseForm = new Date('January 03, 2025 15:23:59');
+let waktuSekarang = new Date();
+
+const idForm = 'idGoogleFormMu';
+
+const formApp = FormApp.openById(idForm)
+
+function deleteTriggers(){
+  const triggers = ScriptApp.getProjectTriggers();
+
+  for (const i = 0; i < triggers.length; i++){
+
+    ScriptApp.deleteTrigger(triggers[i]);
+  }
+  Logger.log('triger sudah didelete')
+}
+
+if(waktuSekarang >= waktuCloseForm) {
+  
+  formApp.setAcceptingResponses(false);
+  Logger.log('Form sudah ditutup cuy.')
+
+  return deleteTriggers();
+} else {
+  formApp.setAcceptingResponses(true);
+  Logger.log('Form masih buka cuy')
+}
+
+}
